d4f1e138752d867749180fd1817c1f7902bf2040
Johan Mathew Joseph <johanmathewjoseph@gmail.com>
2022-09-18 22:31:56 +0530

    🚚 Update Show All Route

--- functions/src/api/index.ts
+++ functions/src/api/index.ts
@@ -1,11 +1,13 @@
 import {Router} from "express";
 import newuserRoute from "./new-user/routes";
 import searchuserRoute from "./search-user/routes";
+import showusersRoute from "./show-users/routes";
 
 export default (): Router => {
   const app = Router();
     
   app.use("/newuser", newuserRoute);
   app.use("/search", searchuserRoute);
+  app.use("/showall", showusersRoute);
   return app;
 };
